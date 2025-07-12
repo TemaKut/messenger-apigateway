eca9c1044e7572a7c92fe1b6c92fcf586348fa2a
Артём Кутузов <tema.kutuzzzov@yandex.ru>
2025-07-12 23:24:13 +0300

    receive socket messages

--- internal/app/handler/websocket/handler.go
+++ internal/app/handler/websocket/handler.go
@@ -1,8 +1,10 @@
 package websocket
 
 import (
+	"fmt"
 	"github.com/TemaKut/messenger-apigateway/internal/app/logger"
 	"golang.org/x/net/websocket"
+	"io"
 )
 
 type Handler struct {
@@ -10,7 +12,9 @@ type Handler struct {
 }
 
 func NewHandler(logger *logger.Logger) *Handler {
-	return &Handler{}
+	return &Handler{
+		logger: logger,
+	}
 }
 
 func (h *Handler) Handle(conn *websocket.Conn) {
@@ -23,4 +27,18 @@ func (h *Handler) Handle(conn *websocket.Conn) {
 			h.logger.Errorf("error closing websocket connection: %s", err)
 		}
 	}()
+
+	for {
+		var message string
+
+		if err := websocket.Message.Receive(conn, &message); err != nil {
+			if err != io.EOF {
+				h.logger.Errorf("error receiving message: %s", err)
+			}
+
+			break
+		}
+
+		fmt.Println(message)
+	}
 }
